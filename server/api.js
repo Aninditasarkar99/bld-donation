a3fac3be73c42cebacc6fa6e50f559ab95e91fe1
Anindita Sarkar <sarkaranindita55@gmail.com>
2023-05-29 22:03:04 +0530

    database push

--- server/api.js
+++ server/api.js
@@ -17,7 +17,7 @@ const port = 5000;
 
 //Mongodb connections
 // const mongoString="mongodb://127.0.0.1:27017";
-const mongoString="mongodb+srv://sarkaranindita55:12345@cluster0.k8y3npq.mongodb.net/"
+const mongoString="mongodb+srv://sarkaranindita55:12345@cluster0.nvwhbh1.mongodb.net/"
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
